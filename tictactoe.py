35ce054c3384fc2574acc13ba8f53a5694ea37aa
Talentedfalcon <82446314+Talentedfalcon@users.noreply.github.com>
2024-12-13 23:48:49 +0400

    quality of life changes

--- tictactoe.py
+++ tictactoe.py
@@ -38,7 +38,21 @@ def check_win(board):
         
         if end:
             text=font.render(f"Winner: {player}",True,"black")
-            break
+            return
+    
+    count=0
+    for i in range(3):
+        for j in range(3):
+            if(board[i][j]["value"]!=''):
+                count+=1
+    if count==9:
+        text=font.render(f"Tie",True,"black")
+        end=1
+
+def reset_board(board):
+    for i in board:
+        for cell in i:
+            cell["value"]=''
 
 board=[]
 
@@ -63,8 +77,10 @@ for i in range(3):
         rect={
             "rect":pygame.Rect(pos[0],pos[1],100,100),
             "color":"white",
+            "border-color":"black",
             "pos":pos,
-            "value":value
+            "value":value,
+            "text-color":"white"
         }
         row.append(rect)
     board.append(row)
@@ -77,30 +93,44 @@ xo_font=pygame.font.SysFont('Time New Roman',xo_font_size)
 turn='X'
 text=font.render(f"Current Turn: {turn}",True,"black")
 
+retry_text=font.render("Retry",True,"black")
+t_width,t_height=retry_text.get_size()
+block_surface=pygame.Surface((1.5*t_width,1.5*t_height),pygame.SRCALPHA)
+b_width,b_height=block_surface.get_size()
+retry_button=None
+
 while running:
     screen.fill("grey")
 
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
-        if event.type==pygame.MOUSEBUTTONDOWN and not end:
+        if event.type==pygame.MOUSEBUTTONDOWN:
             x,y=pygame.mouse.get_pos()
-            for i in board:
-                for cell in i:
-                    if(cell["rect"].collidepoint(x,y)):
-                        if(cell["value"]==''):
-                            cell["value"]=turn
-                            if(turn=='X'):
-                                turn='O'
-                            else:
-                                turn='X'
-                            text=font.render(f"Current Turn: {turn}",True,"black")
-                            check_win(board)
+            if(retry_button and retry_button.collidepoint(x,y)):
+                retry_button=None
+                end=0
+                reset_board(board)
+            elif (not end):
+                for i in board:
+                    for cell in i:
+                        if(cell["rect"].collidepoint(x,y)):
+                            if(cell["value"]==''):
+                                cell["value"]=turn
+                                if(turn=='X'):
+                                    cell["text-color"]="blue"
+                                    turn='O'
+                                else:
+                                    cell["text-color"]="red"
+                                    turn='X'
+                                text=font.render(f"Current Turn: {turn}",True,"black")
+                                check_win(board)
 
     for i in board:
         for cell in i:
             pygame.draw.rect(screen,cell["color"],cell["rect"])
-            cell_text=xo_font.render(cell["value"],True,"blue")
+            pygame.draw.rect(screen,cell["border-color"],cell["rect"],1)
+            cell_text=xo_font.render(cell["value"],True,cell["text-color"])
             text_rect=cell_text.get_rect()
             screen.blit(
                 cell_text,
@@ -111,14 +141,10 @@ while running:
             )
 
     if(end):
-        retry_text=font.render("Retry",True,"black")
-        t_width,t_height=retry_text.get_size()
-        block_surface=pygame.Surface((1.5*t_width,1.5*t_height),pygame.SRCALPHA)
-        b_width,b_height=block_surface.get_size()
         pygame.draw.rect(block_surface,"white",block_surface.get_rect(),border_radius=10)
         pygame.draw.rect(block_surface,"black",block_surface.get_rect(),1,10)
         block_surface.blit(retry_text,(b_width/2-t_width/2,b_height/2-t_height/2))
-        screen.blit(block_surface,(width/2-b_width/2,height/2+1.8*cell_size))
+        retry_button=screen.blit(block_surface,(width/2-b_width/2,height/2+1.8*cell_size))
         
     screen.blit(text,(0,height-40))
 
